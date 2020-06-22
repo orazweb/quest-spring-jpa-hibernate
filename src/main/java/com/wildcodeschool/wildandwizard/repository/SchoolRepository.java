4b8d2a19ad6b46edd5a99c7afbca68b1d0f96f48
orazweb <jer.paoli@gmail.com>
2020-06-22 11:35:00 +0200

    add School CRUD done

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -2,5 +2,10 @@ package com.wildcodeschool.wildandwizard.repository;
 
 // TODO : update this interface
 
-public interface SchoolRepository {
+import com.wildcodeschool.wildandwizard.entity.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SchoolRepository extends JpaRepository<School, Long> {
 }
